c1df94b1a0d9a3e200bfb69271d175f1ab8129ba
Christian Santos <christian@therubythree.com>
2019-06-27 11:31:55 -0700

    added associations with review attribute to current book.id and current user id

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+    before_action :find_book
 
     def new
         @review = Review.new 
@@ -6,11 +7,26 @@ class ReviewsController < ApplicationController
 
     def create 
         @review = Review.new(review_params)
+        @review.book_id = @book.id # sets review attribute to the current book.id
+        @review.user_id = currrent_user.id
+         # line 10-11  - associate a review with the current book and with the current user
+         # @ instance variables/attributes available because created those migrations  
+        if @review.save
+            redirect_to book_path(@book)
+        else 
+          render 'new'
+        end
     end
 
     private
       def review_params
         params.require(:review).permit(:rating, :comment)
+      end 
+
+      def find_book
+        @book = Book.find(params[:book_id])
+        #specify :book_id instead of just :id because we are in the reviews controller
+        #and a review is associated with a book_id 
       end 
 
 end
